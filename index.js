1e5a4746d7bc00bfdf58e06c9864697a8bb3f406
Sanaya James <sanaya.j.james@gmail.com>
2025-09-05 23:52:22 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+console.log("Welcome!;")
+
+const burgers = ['Hamburger', 'Cheeseburger'];
+
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger(){
+    const newBurger = "Flatburger";
+    burgers.push(newBurger); 
+
+    if(true){
+        const anotherNewBurger = 'Maple Bacon Burger';
+        burgers.push(anotherNewBurger);
+    }
+    return burgers;
+}
+
+function changeFeaturedDrink (newDrink){
+    featuredDrink = newDrink;
+    return featuredDrink;
+}
+
+changeFeaturedDrink('The JavaShake');
+console.log(featuredDrink);
+addBurger();
+console.log(burgers);
